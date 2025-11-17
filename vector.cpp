7779a0ad1ceb65a2db5b2c0be87331a1311f658e
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-17 23:39:31 +0600

    Executing the vector

---
+++ vector.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainVector(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(7);
+
+    cout << v[0] << " " << v[1] << endl;
+}
+
+int main(){
+
+    explainVector();
+
+    return 0;
+}
