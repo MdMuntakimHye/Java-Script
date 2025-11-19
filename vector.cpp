1ff73c5cc4069c910ad0ea89fad70a01b46eaadf
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-19 16:41:58 +0600

    added more code to vector

--- vector.cpp
+++ vector.cpp
@@ -9,6 +9,35 @@ void explainVector(){
     v.push_back(7);
 
     cout << v[0] << " " << v[1] << endl;
+
+    v.emplace_back(2);
+    cout << v[4] << endl;
+
+    //pair
+    vector<pair<int,int>>vec;
+    vec.push_back({3,4});
+    vec.emplace_back(1,2);
+
+    cout << vec[0].first << " for pushback " << vec[0].second << endl;
+    cout << vec[1].first << " for emplace back " << vec[1].second << endl;
+
+    vector<int> v1(5,20);
+    vector<int> v2(4,34);
+
+    vector<int>::iterator it = v1.begin();
+    it++;
+    cout << *(it) << " ";
+    for(auto& p : v1){
+        cout << p;
+    }
+    cout << endl;
+
+    //v2
+    for(auto& p: v2){
+        cout << p << " ";
+    }
+
+    
 }
 
 int main(){
